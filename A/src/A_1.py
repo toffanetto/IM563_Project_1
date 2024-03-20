3cdfe05dd928daac2cf6cbc8d46305c0eb737e94
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2024-03-20 14:59:01 -0300

    Reduce completed

--- A/src/A_1.py
+++ A/src/A_1.py
@@ -3,6 +3,28 @@
 import numpy as np
 import imageio.v2 as iio
 
+def imageArrayReduce(image_array, reduce_ratio):
+    n_rows = np.uint16(image_array.shape[0]*(reduce_ratio/100))
+    n_collumns = np.uint16(image_array.shape[1]*(reduce_ratio/100))
+
+    image_reduced = np.zeros([n_rows, n_collumns, 3], dtype=np.uint8)
+
+    ratio_step = np.uint16(1/(reduce_ratio/100))
+
+    for k in range(image_array.shape[2]):
+        for j in range(n_collumns):
+            for i in range(n_rows):
+
+                intensity = 0
+
+                for m in range(ratio_step):
+                    for n in range(ratio_step):
+                        intensity += image_array[i*ratio_step + m, j*ratio_step + n, k]
+
+                image_reduced[i, j, k]  = np.uint8(intensity/(ratio_step*ratio_step))
+                
+    return image_reduced
+
 # Read image from file
 
 image_raw = iio.imread('A/img/test_image-1.jpg')
@@ -13,14 +35,19 @@ print("IMG Tensor size: "+str(image_raw.shape))
 #   The raw image have the size 540x960
 #   Resizing to 50% of original size, we get a 270x480 image
 
-image_reduced_50 = np.zeros([270, 480, 3], dtype=np.uint8)
+image_reduced_50 = imageArrayReduce(image_raw, 50)
 
 print("IMG reduced 50% Tensor size: "+str(image_reduced_50.shape))
 
-for k in range(3):
-    for j in range(480):
-        for i in range(270):
-            aux = np.array([image_raw[i*2, j*2, k], image_raw[i*2+1, j*2, k], image_raw[i*2, j*2+1, k], image_raw[i*2+1, j*2+1, k]])
-            image_reduced_50[i, j, k] = (np.average(aux))
-
 iio.imwrite('A/img/image_reduced_50.jpg', image_reduced_50)
+
+# 1) Resizing img to 25%
+#   The raw image have the size 540x960
+#   Resizing to 25% of original size, we get a 135x240 image
+
+image_reduced_25 = imageArrayReduce(image_raw, 25)
+
+print("IMG reduced 50% Tensor size: "+str(image_reduced_25.shape))
+
+iio.imwrite('A/img/image_reduced_25.jpg', image_reduced_25)
+

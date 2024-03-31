a6449c8b711c80f04abdea06689f6e3d10f485ea
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2024-03-31 16:13:50 -0300

    Hard tring to get distortion parameters

--- 1-Activitie/B/src/deform_correction.py
+++ 1-Activitie/B/src/deform_correction.py
@@ -3,10 +3,9 @@
 import numpy as np
 import imageio.v2 as iio
 import matplotlib.pyplot as plt
+from scipy import signal
 
-BORDER_THRESHOLD = 14
-N_BORDER_SEARCH = 1
-M_BORDER_SEARCH = 1
+ERODE_KERNEL = 1 # Mask (2*ERODE_KERNEL+1, 2*ERODE_KERNEL+1)
 
 def imageArrayReduce(image_array, reduce_ratio):
     n_rows = np.uint16(image_array.shape[0]*(reduce_ratio/100)) # Number of rows of the reduced image
@@ -45,164 +44,185 @@ def rgb2GsArray(image_array, r, g, b):
             
     return imageGS
 
-def getHistogramNorm(image_array):
+
+
+def imageAxisDraw(image_array):
     n_rows = np.uint16(image_array.shape[0]) # Number of rows of the image
     n_collumns = np.uint16(image_array.shape[1]) # Number of collumns of the image
 
-    histogram_array = np.zeros([256]) # Empty arraw for the construction of the histogram
+    for j in range(n_collumns): # Collumn sweep
+        image_array[np.uint16(n_rows/2), j] = 255
+    for i in range(n_rows): # Row sweep
+        image_array[i, np.uint16(n_collumns/2)] = 255
+        
+    return image_array
+
+def binaryImage(image_array):
+    n_rows = np.uint16(image_array.shape[0]) # Number of rows of the image
+    n_collumns = np.uint16(image_array.shape[1]) # Number of collumns of the image
+
+    threshold = 130
 
     for j in range(n_collumns): # Collumn sweep
         for i in range(n_rows): # Row sweep
+            if image_array[i, j] < threshold:
+                image_array[i, j] = 0
+            else:
+                image_array[i, j] = 255
         
-            histogram_array[image_array[i][j]] += 1
-            
-    return (histogram_array/n_collumns)/n_rows
+    return image_array
 
-def getCumulativeHistogram(histogram_array):
-
-    cumulative_histogram_array = np.zeros([256]) # Empty arraw for the construction of the histogram
-
-    cumulative_histogram_array[0] = histogram_array[0]
-
-    for i in range(len(histogram_array)-1):      
-            cumulative_histogram_array[i+1] = cumulative_histogram_array[i] + histogram_array[i+1]
-        
-            
-    return cumulative_histogram_array
-
-def histogramEqualization(image_array, cumulative_histogram):
+def erodeImage(image_array):
     n_rows = np.uint16(image_array.shape[0]) # Number of rows of the image
     n_collumns = np.uint16(image_array.shape[1]) # Number of collumns of the image
 
-    image_equalized = np.zeros([n_rows, n_collumns], dtype=np.uint8)
-
-    for j in range(n_collumns): # Collumn sweep
-        for i in range(n_rows): # Row sweep
-            image_equalized[i, j] = (256-1)*cumulative_histogram[image_array[i, j]]
-
-    return image_equalized
-
-def blurFilter(image_array):
-    n_rows = np.uint16(image_array.shape[0]) # Number of rows of the image
-    n_collumns = np.uint16(image_array.shape[1]) # Number of collumns of the image
-
-    image_blurred = np.zeros([n_rows, n_collumns, 3], dtype=np.uint8)
-
-    blur_matrix = np.array([[2, 4, 5, 4, 2],
-                            [4, 9, 2, 9, 4],
-                            [5, 12, 15, 12, 5],
-                            [4, 9, 12, 9, 4],
-                            [2, 4, 5, 4, 2]])/159
-    
-    n_filter = np.uint16(blur_matrix.shape[0]) # Number of rows of the filter
-    m_filter = np.uint16(blur_matrix.shape[1]) # Number of collumns of the filter
-
-    n_1 = np.int16(np.floor(n_filter/2))
-    m_1 = np.int16(np.floor(m_filter/2))
-    
-    for i in range(n_rows):
-        for j in range(n_collumns):
-            blur_pixel = 0
-            n_pixel = 0
-            for n in range(n_filter):
-                if(i-n_1+n >= 0) and (i-n_1+n < n_rows):
-                    for m in range(m_filter):
-                        if(j-m_1+m >= 0) and (j-m_1+m < n_collumns):
-                            blur_pixel += image_array[i-n_1+n,j-m_1+m]+blur_matrix[n, m]
-                            n_pixel += 1
-
-            image_blurred[i, j] = np.uint8(blur_pixel/n_pixel)
-        print('BLUR | '+str(np.uint8(i/n_rows*100))+'% completed')
-
-    return image_blurred
-
-def gradVector (Ix, Iy):
-    G = np.hypot(Ix,Iy)
-    print(G)
-    G = np.multiply(np.divide(G,np.max(G)),255)
-
-    theta = np.arctan2(Iy,Ix)
-    theta[theta < 0] += np.pi
-
-    return G, theta
-
-def borderDetector(image_array):
-    n_rows = np.uint16(image_array.shape[0]) # Number of rows of the image
-    n_collumns = np.uint16(image_array.shape[1]) # Number of collumns of the image
-
-    image_borders = np.zeros([n_rows, n_collumns], dtype=np.uint8)
-    Ix = np.zeros([n_rows, n_collumns], dtype=np.int32)
-    Iy = np.zeros([n_rows, n_collumns], dtype=np.int32)
-
-    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
-    Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    image_eroded = np.zeros([n_rows, n_collumns], dtype=np.uint8)
 
     for i in range(n_rows):
         for j in range(n_collumns):
-            for n in range(N_BORDER_SEARCH*2+1):
-                if(i-N_BORDER_SEARCH+n >= 0) and (i-N_BORDER_SEARCH+n < n_rows):
-                    for m in range(M_BORDER_SEARCH*2+1):
-                        if(j-M_BORDER_SEARCH+m >= 0) and (j-M_BORDER_SEARCH+m < n_collumns):
-                            Ix[i,j] += Kx[n,m]*image_array[i-N_BORDER_SEARCH+n, j-M_BORDER_SEARCH+m,0]
-                            Iy[i,j] += Ky[n,m]*image_array[i-N_BORDER_SEARCH+n, j-M_BORDER_SEARCH+m,0]
+            e = 0
+            for n in range(ERODE_KERNEL*2+1):
+                if(i-ERODE_KERNEL+n >= 0) and (i-ERODE_KERNEL+n < n_rows):
+                    for m in range(ERODE_KERNEL*2+1):
+                        if(j-ERODE_KERNEL+m >= 0) and (j-ERODE_KERNEL+m < n_collumns):
+                            if(image_array[i,j]>0):
+                                e += image_array[i-ERODE_KERNEL+n, j-ERODE_KERNEL+m] # Getting intensity gradient in x-axis
+            if(e>=((ERODE_KERNEL*2+1)**2)/2*255):
+                image_eroded[i,j] = 255
+        print('Eroding image | '+str(np.uint8(i/n_rows*100))+'% completed')
 
-        print('Gradient image | '+str(np.uint8(i/n_rows*100))+'% completed')
+    return image_eroded
 
-    G, theta = gradVector(Ix=Ix, Iy=Iy)
+def findSquares(image_array):
+    squares = []
+    
+    n_rows = np.uint16(image_array.shape[0]) # Number of rows of the image
+    n_collumns = np.uint16(image_array.shape[1]) # Number of collumns of the image
+
+    K = np.array([[0, 1, 1], 
+                   [1, -1, -1], 
+                   [1, -1, -1]]) 
+    
+    mask_sum = 0
 
     for i in range(n_rows):
-        a = 255
-        b = 255
-        if(i-N_BORDER_SEARCH >= 0) and (i+N_BORDER_SEARCH < n_rows):
-            for j in range(n_collumns):
-                    if(j-M_BORDER_SEARCH >= 0) and (j+M_BORDER_SEARCH < n_collumns):
-
-                        if (theta[i,j] >= 0 and theta[i,j] < np.pi/16) or (theta[i,j] >= np.pi/2-np.pi/16 and theta[i,j] < np.pi/2): # E/W
-                            a = G[i, j+1]
-                            b = G[i, j-1]
-                        elif (theta[i,j] >= np.pi/16 and theta[i,j] < np.pi/16+np.pi/8): # NE/SW
-                            a = G[i+1, j+1]
-                            b = G[i-1, j-1]
-                        elif (theta[i,j] >= np.pi/4-np.pi/16 and theta[i,j] < np.pi/4+np.pi/16): #N/S
-                            a = G[i+1, j]
-                            b = G[i-1, j]
-                        elif (theta[i,j] >= np.pi/4+np.pi/16 and theta[i,j] < np.pi/2-np.pi/16): #NW/SE
-                            a = G[i-1, j+1]
-                            b = G[i-1, j+1]
-
-                        if(G[i,j] >= (max(a,b)) and G[i,j] >= BORDER_THRESHOLD):
-                            image_borders[i,j] = 255
-            
-        print('Thining edges | '+str(np.uint8(i/n_rows*100))+'% completed')
-
-    return image_borders
+        for j in range(n_collumns):
+            mask_sum = 0
+            for n in range(1*2+1):
+                if(i-1+n >= 0) and (i-1+n < n_rows):
+                    for m in range(1*2+1):
+                        if(j-1+m >= 0) and (j-1+m < n_collumns):
+                            mask_sum += K[n,m]*image_array[i-1+n, j-1+m]
+            if(mask_sum/255 == 4):
+                squares.append((j,i))
+    return squares
+  
 
 ######################
 
-#image_raw = iio.imread('B/img/squared_deform.jpeg')
-
-#print("IMG Tensor size: "+str(image_raw.shape))
+#image_raw = iio.imread('B/img/chess.jpeg')
 
 #image_reduced = imageArrayReduce(image_raw, 25)
 
-image_reduced = iio.imread('B/img/output/image_reduced.png')
+#image_gs = rgb2GsArray(image_reduced, 30, 55, 15)
 
-image_gs= rgb2GsArray(image_reduced, 30, 55, 15)
+#iio.imwrite('B/img/output/image_gs.png', image_gs)
 
-iio.imwrite('B/img/output/image_gs.png', image_gs)
+#image_binary = binaryImage(image_gs)
+#image_binary_erode = erodeImage(image_binary)
 
-histogram_raw = getHistogramNorm(image_gs)
+#iio.imwrite('B/img/output/image_binary_erode.png', image_binary_erode)
 
-cumulative_histogram_raw = getCumulativeHistogram(histogram_raw)
+image_binary_erode = iio.imread('B/img/output/image_binary_erode.png')
 
-image_equalized = histogramEqualization(image_gs, cumulative_histogram_raw)
+image_original = iio.imread('B/img/image_original.png')
 
-iio.imwrite('B/img/output/image_equalized.png', image_equalized)
+image_original_binary = binaryImage(image_original)
 
-image_blur = blurFilter(image_equalized)
+iio.imwrite('B/img/output/image_original_binary.png', image_original_binary)
 
-iio.imwrite('B/img/output/image_blur.png', image_blur)
+squares = findSquares(image_binary_erode)
 
-image_borders = borderDetector(image_blur)
+squares_original = findSquares(image_original_binary)
 
-iio.imwrite('B/img/output/image_borders.png', image_borders)
+plt.figure()
+plt.imshow(image_binary_erode, cmap='gray')
+plt.scatter(*zip(*squares))
+plt.xlabel('x-axis [pixels]')
+plt.ylabel('y-axis [pixels]')
+plt.title('Distorted image with markpoints')
+
+plt.savefig("./B/plot/image_binary_erode.pdf", format="pdf", bbox_inches="tight")
+
+plt.figure()
+plt.imshow(image_original_binary, cmap='gray')
+plt.scatter(*zip(*squares_original))
+plt.xlabel('x-axis [pixels]')
+plt.ylabel('y-axis [pixels]')
+plt.title('Original image with markpoints')
+
+plt.savefig("./B/plot/image_original_binary.pdf", format="pdf", bbox_inches="tight")
+
+Id = []
+Iu = []
+
+xd_ = np.ceil(np.uint16(image_original_binary.shape[1])/2)
+yd_ = np.ceil(np.uint16(image_original_binary.shape[0])/2)
+
+for i in range(len(squares_original)):
+    for j in range(len(squares)):
+        if np.sqrt((squares_original[i][0]-squares[j][0])**2 + (squares_original[i][1]-squares[j][1])**2) < 20:
+            Id.append(squares[j])
+            Iu.append(squares_original[i])
+            
+plt.figure()
+Id_marker = plt.scatter(*zip(*Id),marker='o')
+Iu_marker = plt.scatter(*zip(*Iu),marker='x')
+Id__marker = plt.scatter(xd_,yd_,marker='+')
+plt.imshow(image_binary_erode, cmap='gray')
+plt.legend((Id_marker, Iu_marker, Id__marker), ('Distorted position point', 'Original position point', 'Image center'),fontsize=8)
+plt.ylim([80,320])
+plt.xlabel('x-axis [pixels]')
+plt.ylabel('y-axis [pixels]')
+plt.gca().invert_yaxis()
+plt.title('Comparison of original and distorted position of markpoints in image')
+
+plt.savefig("./B/plot/markpoints_original_distorted.pdf", format="pdf", bbox_inches="tight")
+
+print(len(Id))
+print(len(squares_original))
+print(len(squares))
+
+K = []
+
+## FIND THE VALUE OF K AND P
+
+for i in range(len(Id)):
+    K.append((Iu[i][0]/(Id[i][0]-xd_) -1 )/(((Id[i][0]-xd_)**2 + (Id[i][1]-yd_)**2)))
+
+K = np.average(K)
+
+Iu_correct = []
+
+## COMPUTE THE DISTOTION CORRECTED POINTS
+
+print(Iu_correct)
+
+# plt.figure()
+# Id_marker = plt.scatter(*zip(*Id),marker='o')
+# Iu_marker = plt.scatter(*zip(*Iu),marker='x')
+# plt.scatter(*zip(*Iu_correct),marker='+')
+# Id__marker = plt.scatter(xd_,yd_,marker='+')
+# plt.imshow(image_binary_erode, cmap='gray')
+# plt.legend((Id_marker, Iu_marker, Id__marker), ('Distorted position point', 'Original position point', 'Image center'),fontsize=8)
+# plt.ylim([80,320])
+# plt.xlabel('x-axis [pixels]')
+# plt.ylabel('y-axis [pixels]')
+# plt.gca().invert_yaxis()
+# plt.title('Comparison of original and distorted position of markpoints in image')
+
+
+## COMPUTE THE DISTOTION CORRECTED IMAGE
+
+plt.show()
+
---
+++ 1-Activitie/B/src/deform_correction_cv2.py
@@ -0,0 +1,49 @@
+# @toffanetto
+
+import numpy as np
+import cv2
+import os
+import glob
+
+chessboard_size = (7,7)
+
+objp = np.zeros((np.prod(chessboard_size),3), dtype=np.float32)
+objp[:, :2] = np.indices(chessboard_size).T.reshape(-1, 2)
+
+object_points = []
+image_points = []
+image_file = './B/img/chess.jpeg'
+
+# Load and process each image
+image = cv2.imread(image_file)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+print(gray.shape)
+# Detect chessboard corners
+ret, corners = cv2.findChessboardCorners(gray, chessboard_size, None)
+print(ret)
+# If corners are found, add object and image points
+if ret:
+    object_points.append(objp)
+    image_points.append(corners)
+    # Draw and display the corners
+    cv2.drawChessboardCorners(image, chessboard_size, corners, ret)
+    cv2.imshow('img', image)
+    cv2.waitKey(500)
+    
+#cv2.destroyAllWindows()
+# Calibrate the camera
+ret, K, dist, rvecs, tvecs = cv2.calibrateCamera(
+    object_points, image_points, gray.shape[::-1], None, None
+)
+print("Calibration matrix K:\n", K)
+print("Distortion:", dist.ravel())
+
+# Load a test image
+test_image = cv2.imread(image_file)
+# Correct the image distortion
+undistorted_image = cv2.undistort(test_image, K, dist, None, K)
+# Display the original and corrected image side by side
+combined_image = np.hstack((test_image, undistorted_image))
+cv2.imshow('Original vs Undistorted', combined_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
